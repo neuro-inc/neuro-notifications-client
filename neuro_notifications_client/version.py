f917d5604f73d548db093ac83018d0c7d17c61ba
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-10-19 18:46:44 +0300

    Remove recursion

--- neuro_notifications_client/version.py
+++ neuro_notifications_client/version.py
@@ -1,4 +1,10 @@
 from importlib.metadata import version
+from typing import Any
 
 
-VERSION = version(__package__)
+def __getattr__(name: str) -> Any:
+    if name == "VERSION":
+        global VERSION
+        VERSION = version(__package__)
+    else:
+        raise AttributeError(f"Unknown attribute {name}")
