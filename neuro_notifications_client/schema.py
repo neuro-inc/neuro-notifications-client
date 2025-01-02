956c642fd58a96e44ef689cd82d24aa2b5357333
parikls <porovozls@gmail.com>
2025-01-02 12:30:56 +0200

    ENG-520: update schema

--- neuro_notifications_client/schema.py
+++ neuro_notifications_client/schema.py
@@ -90,18 +90,10 @@ class CreditsWillRunOutSoonSchema(Schema):
 class OrgCreditsWillRunOutSoonSchema(Schema):
     org_name = fields.String(required=True)
     credits = fields.Decimal(required=True, as_string=True)
-    balance_projection_intervals = fields.List(
-        fields.Integer(
-            validate=[validate.Range(min=0)]
-        ),
+    exceeds_interval_seconds = fields.Integer(
         required=True,
         allow_none=False,
-        validate=[validate.Length(min=1)],
-    )
-    current_interval_index = fields.Integer(
-        required=True,
-        allow_none=False,
-        validate=[validate.Range(min=0)],
+        validate=[validate.Range(min=0)]
     )
 
     @post_load
